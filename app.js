6d3e06f06738392ef8387cf5d274daec7c064eb5
treguv <vladislavtregubov00@gmail.com>
2020-11-11 22:01:18 -0800

    imported and used inquirer

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
+// const fs = require("fs");
 
-const generatePage = require("./src/page-template.js");
+// const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
-  console.log("portfolio complete!");
-});
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
+// fs.writeFile("index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
+//   console.log("portfolio complete!");
+// });
+// console.log(inquirer);
